6753dbe6be6ba1dbe4dcef8122fc824efd2e3004
Anton Rychkov <antrychkov@gmail.com>
2024-03-25 22:27:45 +0500

    9.1 Добавил обработчики для открытия формы

---
+++ js/form.js
@@ -0,0 +1,30 @@
+import { isEscKey } from './util.js';
+
+const uploadImgInput = document.querySelector('.img-upload__input');
+const uploadImgForm = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+
+
+function onDocumentKeydown(evt) {
+  if (isEscKey(evt)) {
+    evt.preventDefault();
+    closeUploadModal();
+  }
+}
+
+function openUploadModal() {
+  uploadImgForm.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closeUploadModal() {
+  uploadImgForm.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+uploadImgInput.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  openUploadModal();
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { photosDataset } from './data.js';
 import { renderPhotos } from './thumbnails.js';
 import './bigPictureRenderer.js';
+import './form.js';
 
 renderPhotos(photosDataset);
