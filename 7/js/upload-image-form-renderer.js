7cad9c37fba8fa33f92afccb0cd17c274862e3a1
Keks <keks@htmlacademy.ru>
2024-03-30 11:21:21 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/upload-image-form-renderer.js
@@ -0,0 +1,46 @@
+import { isEscKey } from './util.js';
+import './upload-image-form-validator.js';
+
+const uploadImgInput = document.querySelector('.img-upload__input');
+const uploadImgForm = document.querySelector('.img-upload__overlay');
+const uploadFormCancelBtn = uploadImgForm.querySelector('.img-upload__cancel');
+const hashTagsInput = uploadImgForm.querySelector('.text__hashtags');
+const commentInput = uploadImgForm.querySelector('.text__description');
+const body = document.querySelector('body');
+
+
+function onDocumentKeydown(evt) {
+  if (isEscKey(evt)) {
+    if (document.activeElement === hashTagsInput || document.activeElement === commentInput) {
+      return;
+    }
+    evt.preventDefault();
+    closeUploadModal();
+  }
+}
+
+function openUploadModal() {
+  uploadImgForm.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+  uploadFormCancelBtn.addEventListener('click', () => {
+    closeUploadModal();
+  });
+}
+
+function closeUploadModal() {
+  resetFormFields();
+  uploadImgForm.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+uploadImgInput.addEventListener('change', () => {
+  openUploadModal();
+});
+
+function resetFormFields() {
+  uploadImgInput.value = '';
+  hashTagsInput.value = '';
+  commentInput.value = '';
+}
