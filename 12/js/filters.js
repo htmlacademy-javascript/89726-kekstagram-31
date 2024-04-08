35b3539d6afa8bf3769d9a52fda187aa034162d9
Keks <keks@htmlacademy.ru>
2024-04-08 11:49:22 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/filters.js
@@ -0,0 +1,72 @@
+import { shuffleArray } from './util.js';
+
+const RANDOM_FILTER_COULT = 10;
+
+const filters = document.querySelector('.img-filters');
+const defaultFilter = filters.querySelector('#filter-default');
+const randomFilter = filters.querySelector('#filter-random');
+const discussedFilter = filters.querySelector('#filter-discussed');
+const picturesContainer = document.querySelector('.pictures');
+
+let photos = [];
+
+window.addEventListener('load', showFilters);
+
+function showFilters() {
+  filters.classList.remove('img-filters--inactive');
+}
+
+function handleDefaultFilter() {
+  resetHiddenPhotos();
+  replacePhotos(photos);
+}
+
+function replacePhotos(photoList) {
+  photoList.forEach((photoElement) => {
+    picturesContainer.removeChild(photoElement);
+  });
+
+  photoList.forEach((photoElement) => {
+    picturesContainer.appendChild(photoElement);
+  });
+}
+
+function handleRandomFilter() {
+  const photosCopy = photos.slice();
+  const randomPhotos = shuffleArray(photosCopy).slice(0, RANDOM_FILTER_COULT);
+
+  photos.forEach((photoElement) => {
+    if(!randomPhotos.includes(photoElement)) {
+      photoElement.classList.add('hidden');
+    }
+  });
+}
+
+function handleDiscussedFilter() {
+  resetHiddenPhotos();
+  const photoElements = Array.from(picturesContainer.querySelectorAll('.picture'));
+
+  photoElements.sort((a, b) => {
+    const commentsA = a.querySelector('.picture__comments').textContent;
+    const commentsB = b.querySelector('.picture__comments').textContent;
+    return commentsB - commentsA;
+  });
+
+  replacePhotos(photoElements);
+}
+
+function resetHiddenPhotos() {
+  photos.forEach((photoElement) => {
+    photoElement.classList.remove('hidden');
+  });
+}
+
+function handlePhotoFilters() {
+  photos = Array.from(picturesContainer.querySelectorAll('.picture'));
+
+  defaultFilter.addEventListener('click', handleDefaultFilter);
+  randomFilter.addEventListener('click', handleRandomFilter);
+  discussedFilter.addEventListener('click', handleDiscussedFilter);
+}
+
+export { handlePhotoFilters };
---
+++ 12/js/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { getData } from './api.js';
+import { handlePictureClick } from './big-picture-renderer.js';
+import { renderPhotos } from './thumbnails.js';
+import { showAlert } from './util.js';
+import { handlePhotoFilters } from './filters.js';
+import './form-renderer.js';
+import './scaler.js';
+import './effector.js';
+
+getData()
+  .then((photos) => {
+    renderPhotos(photos);
+    handlePictureClick(photos);
+    handlePhotoFilters();
+  })
+  .catch((err) => {
+    showAlert(err.message);
+  });
+
